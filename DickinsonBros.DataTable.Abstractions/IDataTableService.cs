05bd2b9d1ff94f1b7e26851290709dac92045bae
Mark Dickinson <marksamdickinson@gmail.com>
2020-09-06 08:57:29 -0700

    Add project files.

---
+++ DickinsonBros.DataTable.Abstractions/IDataTableService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DickinsonBros.DataTable.Abstractions
+{
+    public interface IDataTableService
+    {
+        System.Data.DataTable ToDataTable<T>(IEnumerable<T> enumerable, string tableName);
+    }
+}
